2dd766fa8dec322863d70ed0f67d32adb414c02a
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-12-07 22:03:26 -0500

    Task 1: A Route Handler for User Registration, Logon, and Logoff

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const dogsRouter = require('./routes/dogs');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const dogsRouter = require("./routes/dogs");
+const userRouter = require("./routes/userRoutes");
+
+global.user_id = null;
+global.users = [];
+global.tasks = [];
 
 const app = express();
+app.use(express.json({ limit: "1kb" }));
 
 // Your middleware here
+app.use("/api/users", userRouter);
 
-app.use('/', dogsRouter); // Do not remove this line
+app.use("/", dogsRouter); // Do not remove this line
+app.post("/api/users", (req, res) => {
+  const newUser = { ...req.body }; // this makes a copy
+  global.users.push(newUser);
+  global.user_id = newUser; // After the registration step, the user is set to logged on.
+  delete req.body.password;
+  res.status(201).json(req.body);
+});
 
-const server =	app.listen(3000, () => console.log("Server listening on port 3000"));
+const server = app.listen(3000, () =>
+  console.log("Server listening on port 3000")
+);
 module.exports = server;
