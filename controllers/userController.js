fcfa6d15fb8b97c7bc04db708e8078068269dd82
padmaja-ramesh <padmaja-ramesh@users.noreply.github.com>
2025-12-15 22:06:26 -0500

    task-1 completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,22 @@
 const { StatusCodes } = require("http-status-codes");
 
-///controllers/userController.js with functions logon, register, and logoff
+global.users = global.users || [];
+global.user_id = global.user_id || null;
 
 function register(req, res) {
+  if (!req.body) {
+    return res.status(400).json({ message: "Request body is required" });
+  }
+
+  global.users = global.users || [];
+  global.user_id = global.user_id || null;
+
   const newUser = { ...req.body };
   global.users.push(newUser);
   global.user_id = newUser;
-  delete req.body.password;
-  res.status(StatusCodes.CREATED).json(req.body);
+
+  const { password, ...safeUser } = newUser; // safer than deleting directly
+  return res.status(StatusCodes.CREATED).json(safeUser);
 }
 
 function logon(req, res) {
@@ -17,16 +26,16 @@ function logon(req, res) {
       user.email === userDetail.email && user.password === userDetail.password
   );
   if (checkUser) {
-    const userName = checkUser.name;
     global.user_id = checkUser;
-    res.status(StatusCodes.OK).json({ name: userName });
+    res.status(StatusCodes.OK).json({ name: checkUser.name });
   } else {
-    res.status(StatusCodes.UNAUTHORIZED);
+    res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid credentials" });
   }
 }
 
 function logoff(req, res) {
-  res.status(StatusCodes.OK);
+  global.user_id = null;
+  res.status(StatusCodes.OK).json({ message: "Logged off" });
 }
 
-module.exports = { register, logoff, logon };
+module.exports = { register, logon, logoff };
